6eb12e98fadaac777ff426dd685ea563d5ef8137
Pierluigi98-Bisogno <pierbisogno98@gmail.com>
2025-08-05 09:52:30 +0200

    data

--- Evento.java
+++ Evento.java
@@ -1,10 +1,12 @@
 public class Evento {
     // Proprietà
     private String titolo;
+    private String data;
     
     // Costruttore
-    public Evento(String titolo) {
+    public Evento(String titolo, String data) {
         this.titolo = titolo;
+        this.data = data;
     }
     
     // Getter
@@ -17,9 +19,19 @@ public class Evento {
         this.titolo = titolo;
     }
     
+    // Getter per data
+    public String getData() {
+        return data;
+    }
+    
+    // Setter per data
+    public void setData(String data) {
+        this.data = data;
+    }
+    
     // Metodo toString per visualizzare l'evento
     @Override
     public String toString() {
-        return "Evento: " + titolo;
+        return "Evento: " + titolo + " - Data: " + data;
     }
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         // Creazione di alcuni eventi di esempio
-        Evento concerto = new Evento("Concerto di Rock");
-        Evento conferenza = new Evento("Conferenza sulla Tecnologia");
-        Evento spettacolo = new Evento("Spettacolo Teatrale");
+        Evento concerto = new Evento("Concerto di Rock", "2024-06-15");
+        Evento conferenza = new Evento("Conferenza sulla Tecnologia", "2024-07-20");
+        Evento spettacolo = new Evento("Spettacolo Teatrale", "2024-08-10");
         
         // Stampa degli eventi
         System.out.println(concerto);
@@ -12,9 +12,11 @@ public class Main {
         
         // Test dei metodi getter e setter
         System.out.println("\nTitolo del primo evento: " + concerto.getTitolo());
+        System.out.println("Data del primo evento: " + concerto.getData());
         
-        // Modifica del titolo
+        // Modifica del titolo e della data
         concerto.setTitolo("Concerto di Jazz");
-        System.out.println("Titolo modificato: " + concerto.getTitolo());
+        concerto.setData("2024-06-22");
+        System.out.println("Evento modificato: " + concerto);
     }
 }
